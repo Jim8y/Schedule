0363dd3f5ee2a5195a89fdc093b34825d97d13b8
Liaojinghui <1498262377@qq.com>
2015-09-11 00:08:33 +0800

    完全修改好后的版本

---
+++ Schedule/schedule/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Schedule
+//
+//  Created by 廖京辉 on 15/9/11.
+//  Copyright (c) 2015年 廖京辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
+
